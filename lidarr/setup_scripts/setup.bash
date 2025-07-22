2117b3fae26ea89750874271222d1cb32483399f
prvctech <103532453+prvctech@users.noreply.github.com>
2025-07-22 14:25:25 -0400

    Update setup.bash

--- lidarr/setup_scripts/setup.bash
+++ lidarr/setup_scripts/setup.bash
@@ -22,9 +22,9 @@ cp -r "$TMP_DIR/Arrbit-main/universal/helpers"     "$DEST_DIR/helpers"
 cp -r "$TMP_DIR/Arrbit-main/universal/connectors"  "$DEST_DIR/connectors"
 
 # Copy lidarr scripts and config folders
-cp -r "$TMP_DIR/Arrbit-main/lidarr/process_scripts/modules"       "$DEST_DIR/process_scripts/modules"
-cp -r "$TMP_DIR/Arrbit-main/lidarr/process_scripts/services"      "$DEST_DIR/process_scripts/services"
-cp -r "$TMP_DIR/Arrbit-main/lidarr/process_scripts/setup_scripts" "$DEST_DIR/process_scripts/setup_scripts"
+cp -r "$TMP_DIR/Arrbit-main/lidarr/process_scripts/modules"       "$DEST_DIR/modules"
+cp -r "$TMP_DIR/Arrbit-main/lidarr/process_scripts/services"      "$DEST_DIR/services"
+cp -r "$TMP_DIR/Arrbit-main/lidarr/process_scripts/setup_scripts" "$DEST_DIR/setup_scripts"
 cp -r "$TMP_DIR/Arrbit-main/lidarr/config"                        "$DEST_DIR/config"
 
 chmod -R 777 "$DEST_DIR"
