54d770d99ead20fb07330134631cbf02c8fc22f6
prvctech <103532453+prvctech@users.noreply.github.com>
2025-07-24 12:45:08 -0400

    Update autoconfig.bash

--- lidarr/process_scripts/services/autoconfig.bash
+++ lidarr/process_scripts/services/autoconfig.bash
@@ -21,7 +21,7 @@ LOG_FILE="$LOG_DIR/arrbit-${SCRIPT_NAME}-$(date +%Y_%m_%d-%H_%M).log"
 touch "$LOG_FILE" && chmod 777 "$LOG_FILE"
 
 # Banner (only first line with color is allowed)
-echo -e "${CYAN}[Arrbit]${NC} ${GREEN}${SCRIPT_NAME} service${NC} ${SCRIPT_VERSION} ..."
+echo -e "${CYAN}[Arrbit]${NC} ${GREEN}Starting ${SCRIPT_NAME} service${NC} ${SCRIPT_VERSION} ..."
 echo
 
 # ----------------------------------------------------------------------------
