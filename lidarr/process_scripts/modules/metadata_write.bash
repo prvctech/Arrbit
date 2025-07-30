a39ad7877a1bfdcdfaa87f7f9e86abbedb38d95b
prvctech <103532453+prvctech@users.noreply.github.com>
2025-07-30 00:01:21 -0400

    Update metadata_write.bash

--- lidarr/process_scripts/modules/metadata_write.bash
+++ lidarr/process_scripts/modules/metadata_write.bash
@@ -53,6 +53,6 @@ else
   log_error "Metadata Write API call failed (response did not validate, check ARR API connectivity and payload)"
 fi
 
-log_info "Done with ${SCRIPT_NAME} module!"
+#log_info "Done with ${SCRIPT_NAME} module!"
 log_info "Log saved to $LOG_FILE"
 exit 0
